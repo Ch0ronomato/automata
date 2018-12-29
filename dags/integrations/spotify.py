826ae7d777c7c90bffe1ad12864f108a4a923dac
Ian Schweer <schweer@adobe.com>
2018-12-28 21:10:49 -0800

    Finish spotify playlist creation

--- dags/integrations/spotify.py
+++ dags/integrations/spotify.py
@@ -1,6 +1,6 @@
 import spotipy
 import spotipy.util as util
-from datetime import date
+from datetime import date, datetime
 
 class SpotifyAPI:
     def __init__(self):
@@ -19,17 +19,45 @@ class SpotifyAPI:
 
     
     def find_album_links(self, songs_by_bands):
+        album_ids = []
         for song_by_band in songs_by_bands:
             query = "track:{song} artist:{band}".format(**song_by_band)
             results = self.sp.search(q=query)
-            # for tracks in results["tracks"]["items"]:
+            if len(results["tracks"]["items"]) == 0:
+                continue
+            album_ids.append(self._get_oldest_matched_album(results))
+            print("Found album {album} for {band}, {song}".format(
+                album=album_ids[-1], 
+                band=song_by_band["band"],
+                song=song_by_band["song"],
+            ))
+        return album_ids
 
-    def create_playlist(self, spotify_album_ids, reddit_name="progmetal"):
-        date_string = date.today().strftime('%Y-%m-%d')
-        playlist = self.sp.user_playlist_create(self.user, f"r/{reddit_name}-{date_string}", public=False)
-        for album_id in spotify_album_ids:
-            tracks = self.sp.album_tracks(album_id)
-            self.sp.user_playlist_add_tracks(self.user, playlist["id"], [track["id"] for track in tracks["items"]])
-        with open("/opt/playlist.json", "w+") as f:
-            import json
-            f.write(json.dumps(playlist))
+    def _get_oldest_matched_album(self, results):
+        date_format = '%Y-%m-%d'
+        first_released_album = 0
+        for i, track in enumerate(results["tracks"]["items"][1:]):
+            oldest_track = results["tracks"]["items"][first_released_album]
+            current_track_release_date = datetime.strptime(track["album"]["release_date"], date_format)
+            oldest_track_release_date = datetime.strptime(oldest_track["album"]["release_date"], date_format)
+            if current_track_release_date < oldest_track_release_date:
+                first_released_album = i + 1
+        return results["tracks"]["items"][first_released_album]["album"]["id"]
+
+
+    def create_playlist(self, playlist_name, spotify_album_ids):
+        all_tracks = [track for album_id in spotify_album_ids for track in self.sp.album_tracks(album_id).pop("items")]
+        existing_playlists = self.sp.user_playlists(self.user).pop("items")
+        if any([playlist["name"] == playlist_name for playlist in existing_playlists]):
+            matched_playlists = [playlist for playlist in existing_playlists if playlist["name"] == playlist_name]
+            if len(matched_playlists) > 1:
+                raise RuntimeError("Too many playlists matched name {name}, {playlists}".format(
+                    name=playlist_name,
+                    playlists=",".join([x["name"] for x in matched_playlists])
+                ))
+            playlist = matched_playlists.pop()
+            playlist_tracks = {x["track"]["id"] for x in self.sp.user_playlist_tracks(self.user, playlist["id"]).pop("items")}
+            all_tracks = filter(lambda track: track["id"] not in playlist_tracks, all_tracks)
+        else:
+            playlist = self.sp.user_playlist_create(self.user, playlist_name, public=False)
+        self.sp.user_playlist_add_tracks(self.user, playlist["id"], [track["id"] for track in all_tracks])
--- test/dags/test_spotify.py
+++ test/dags/test_spotify.py
@@ -3,10 +3,90 @@ from unittest.mock import Mock, call, patch, PropertyMock
 from dags.integrations.spotify import SpotifyAPI
 
 class TestSpotify(unittest.TestCase):
-    def setUp(self):
+    @patch("spotipy.util.prompt_for_user_token", return_value=True)
+    def setUp(self, _):
         self.unit = SpotifyAPI()
 
-    def test_playground(self):
-        # self.unit.find_album_links([{'band': 'Coheed and Cambria', 'song': 'Far'}])
-        self.unit.create_playlist(["spotify:album:1CMw0rLUVCNjvPQp7UKyhv"])
+    @patch("spotipy.Spotify")
+    @patch("spotipy.util.prompt_for_user_token", return_value=True)
+    def test_find_album_matches_links_for_tracks_items_with_band_song_pair(self, auth_mock, spotify_mock):
+        mock_response = """{
+            "tracks": {
+                "items": [{
+                    "name": "Some Song",
+                    "album": {
+                        "id": "12345",
+                        "release_date": "2018-01-01"
+                    },
+                    "artists": [{
+                        "name": "Some Artist"
+                    }]
+                }]
+            }
+        }
+        """
+        import json
+        spotify_mock().search = Mock(name="search mock", return_value=json.loads(mock_response))
+        api = SpotifyAPI()
+        self.assertEqual(api.find_album_links([{"band": "Some Artist", "song": "Some Song"}]), ["12345"])
 
+    @patch("spotipy.Spotify")
+    @patch("spotipy.util.prompt_for_user_token", return_value=True)
+    def test_find_album_links_picks_first_release_date_for_ambiguity(self, auth_mock, spotify_mock):
+        mock_response= """{
+            "tracks": {
+                "items": [{
+                    "name": "Some Song",
+                    "album": {
+                        "id": "12345",
+                        "release_date": "2018-01-01"
+                    },
+                    "artists": [{
+                        "name": "Some Artist"
+                    }]
+                },{
+                    "name": "Some Song",
+                    "album": {
+                        "id": "2345",
+                        "release_date": "2019-01-01"
+                    },
+                    "artists": [{
+                        "name": "Some Artist"
+                    }]
+                }]
+            }
+        }
+        """
+        import json
+        spotify_mock().search = Mock(name="search mock", return_value=json.loads(mock_response))
+        api = SpotifyAPI()
+        self.assertEqual(api.find_album_links([{"band": "Some Artist", "song": "Some Song"}]), ["12345"])
+    
+    @patch("spotipy.Spotify")
+    @patch("spotipy.util.prompt_for_user_token", return_value=True)
+    def test_create_playlists_adds_album_tracks_to_new_playlists(self, auth_mock, spotify_mock):
+        spotify_mock().user_playlist_create = Mock(return_value={"id": "12345"})
+        spotify_mock().album_tracks = Mock(return_value={"items": [{"id": "track1"}, {"id": "track2"}]})
+        api = SpotifyAPI()
+        api.user = "abc123"
+        api.create_playlist("some_playlist", ["spotify:album:12345"])
+        spotify_mock().user_playlist_add_tracks.assert_any_call("abc123", "12345", ["track1", "track2"])
+
+    @patch("spotipy.Spotify")
+    @patch("spotipy.util.prompt_for_user_token", return_value=True)
+    def test_create_playlists_adds_new_tracks_to_existing_playlists(self, auth_mock, spotify_mock):
+        spotify_mock().user_playlists = Mock(return_value={"items": [{"id": "12345","name": "playlist1234"}]})
+        spotify_mock().album_tracks = Mock(return_value={"items": [{"id": "track1"}, {"id": "track2"}]})
+        spotify_mock().user_playlist_tracks = Mock(return_value={"items": [{"track": {"id": "track1"}}]})
+        api = SpotifyAPI()
+        api.user = "abc123"
+        api.create_playlist("playlist1234", ["spotify:album:12345"])
+        spotify_mock().user_playlist_add_tracks.assert_any_call("abc123", "12345", ["track2"])
+
+    @patch("spotipy.Spotify")
+    @patch("spotipy.util.prompt_for_user_token", return_value=True)
+    def test_create_playlists_errors_when_multiple_playlists_match(self, auth_mock, spotify_mock):
+        spotify_mock().user_playlists = Mock(return_value={"items": [{"id": "23456","name": "playlist1234"}, {"id": "12345","name": "playlist1234"}]})
+        api = SpotifyAPI()
+        api.user = "abc123"
+        self.assertRaises(RuntimeError, api.create_playlist, "playlist1234", ["spotify:album:12345"])
