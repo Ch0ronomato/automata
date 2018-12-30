2d2fa38179bffd1c04c2207c8853fac4897beb6c
Ian Schweer <schweer@adobe.com>
2018-12-30 11:32:53 -0800

    Add basic integration test

--- pavement.py
+++ pavement.py
@@ -9,8 +9,25 @@ from profilehooks import timecall
 def test_dags():
     """Execute Python test suite and generate a code coverage report
     """
-    sh('python -m pytest --junitxml=/tmp/tests.xml --cov=dag --cov=util --cov-report xml:/tmp/coverage.xml --cov-report term-missing:skip-covered -v test/')
+    sh('python -m pytest --junitxml=/tmp/tests.xml --cov=dags --cov=util --cov-report xml:/tmp/coverage.xml --cov-report term-missing:skip-covered -v test/')
 
 @task
 def airflow():
     sh('./entrypoint.sh webserver')
+
+@task
+@consume_args
+def makes_a_playlist(args):
+    if not args:
+        raise RuntimeError("Give me a date")
+    date = args[0]
+    from dags.integrations.reddit import CurrentDaysBands
+    from dags.integrations.spotify import SpotifyAPI
+    spotify = SpotifyAPI()
+    reddit = CurrentDaysBands()
+    songs_posted = reddit.get_bands(date)
+    print("Found songs {}".format(songs_posted))
+    spotify_albums = spotify.find_album_links(songs_posted)
+    print("Found albums {}".format(spotify_albums))
+    spotify.create_playlist("r/progmetal-" + date, spotify_albums)
+    print("Check spotify?")
