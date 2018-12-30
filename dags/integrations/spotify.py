c70300cbee18f837a09aab24712ae6bc35d604e3
Ian Schweer <schweer@adobe.com>
2018-12-29 21:18:25 -0800

    Switch to public playlist fetching

--- dags/integrations/spotify.py
+++ dags/integrations/spotify.py
@@ -4,7 +4,7 @@ from datetime import date, datetime
 
 class SpotifyAPI:
     def __init__(self):
-        scope = 'playlist-modify-private user-follow-read'
+        scope = 'playlist-modify-public playlist-modify-private user-follow-read'
         self.user = '1237967510'
         token = util.prompt_for_user_token(self.user, scope, client_id='80dbe1a0f3144ee39a55e4faaa5e1aea', client_secret='0e14e6cc3e7f4c05ac8cc49cde3a5df3', redirect_uri="http://localhost:3333")
 
@@ -13,9 +13,7 @@ class SpotifyAPI:
             self.sp = spotipy.Spotify(auth=token)
         else:
             raise RuntimeError("Couldn't authorize")
-        # user_playlist_create 
         # current_user_followed_artists 
-        # user_playlist_add_tracks 
 
     
     def find_album_links(self, songs_by_bands):
@@ -46,18 +44,39 @@ class SpotifyAPI:
 
 
     def create_playlist(self, playlist_name, spotify_album_ids):
-        all_tracks = [track for album_id in spotify_album_ids for track in self.sp.album_tracks(album_id).pop("items")]
-        existing_playlists = self.sp.user_playlists(self.user).pop("items")
-        if any([playlist["name"] == playlist_name for playlist in existing_playlists]):
-            matched_playlists = [playlist for playlist in existing_playlists if playlist["name"] == playlist_name]
-            if len(matched_playlists) > 1:
-                raise RuntimeError("Too many playlists matched name {name}, {playlists}".format(
-                    name=playlist_name,
-                    playlists=",".join([x["name"] for x in matched_playlists])
-                ))
+        all_tracks = [
+            track
+            for album_id
+            in spotify_album_ids
+            for track
+            in self.sp.album_tracks(album_id).pop("items")
+        ]
+        matched_playlists = [
+            playlist
+            for playlist
+            in self.sp.user_playlists(self.user).pop("items")
+            if playlist["name"] == playlist_name
+        ]
+        if len(matched_playlists) > 1:
+            raise RuntimeError("Too many playlists matched name {name}, {playlists}".format(
+                name=playlist_name,
+                playlists=",".join([x["name"] for x in matched_playlists])
+            ))
+        elif len(matched_playlists) == 1:
             playlist = matched_playlists.pop()
-            playlist_tracks = {x["track"]["id"] for x in self.sp.user_playlist_tracks(self.user, playlist["id"]).pop("items")}
+            playlist_tracks = {
+                x["track"]["id"]
+                for x
+                in self.sp.user_playlist_tracks(self.user, playlist["id"]).pop("items")
+            }
             all_tracks = filter(lambda track: track["id"] not in playlist_tracks, all_tracks)
         else:
-            playlist = self.sp.user_playlist_create(self.user, playlist_name, public=False)
-        self.sp.user_playlist_add_tracks(self.user, playlist["id"], [track["id"] for track in all_tracks])
+            playlist = self.sp.user_playlist_create(self.user, playlist_name)
+        self._add_tracks_to_playlist(playlist["id"], all_tracks)
+
+    def _add_tracks_to_playlist(self, playlist_id, all_tracks):
+        track_ids = [track["id"] for track in all_tracks]
+        if not track_ids:
+            print("No tracks are new, nothing to do")
+            return
+        self.sp.user_playlist_add_tracks(self.user, playlist_id, track_ids)
